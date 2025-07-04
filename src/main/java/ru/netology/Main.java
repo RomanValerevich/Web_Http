9e725114c112f8ada7d84284ebe53d072e549996
RomanValerevich <rvhorol@gmail.com>
2025-07-04 11:40:12 +0400

    Refactoring Main

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,18 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Server server = new Server(64);
+        server.start(9999);
     }
 }
